64d52454072013a85cce518a9ca55ef31e9f7d5b
nikhilroy2 <nikhilchandraroypoet@gmail.com>
2020-04-28 21:14:42 +0600

    Updates

---
+++ precache-manifest.a43d5dfbbdf3af23079c8965f22e8407.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6752ec34e84b8bf2ed8a30ad41dae64d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e90e9b57c4bd71fc9d4b",
+    "url": "./static/css/main.b345cbab.chunk.css"
+  },
+  {
+    "revision": "0b15209ab2fffe2c330d",
+    "url": "./static/js/2.5e76adf5.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "./static/js/2.5e76adf5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e90e9b57c4bd71fc9d4b",
+    "url": "./static/js/main.4c39dc53.chunk.js"
+  },
+  {
+    "revision": "1d1feb9ac959fa64e717",
+    "url": "./static/js/runtime-main.33d7aa7f.js"
+  },
+  {
+    "revision": "1a2a4a6b42a2c627128e1f09c3657e63",
+    "url": "./static/media/img1.1a2a4a6b.png"
+  },
+  {
+    "revision": "579de8a3eed9d594b741b8b5e9380053",
+    "url": "./static/media/img2.579de8a3.png"
+  },
+  {
+    "revision": "2093a58e9a5cb2f2b328f219e4255800",
+    "url": "./static/media/img3.2093a58e.png"
+  },
+  {
+    "revision": "e421e0ff93b9931944ecbc70238d2020",
+    "url": "./static/media/img4.e421e0ff.png"
+  },
+  {
+    "revision": "112ef05fdec8110491c3ecc13b9e8baf",
+    "url": "./static/media/img5.112ef05f.png"
+  },
+  {
+    "revision": "3711527b7ccdd00908bb9d612afdbffe",
+    "url": "./static/media/img6.3711527b.png"
+  },
+  {
+    "revision": "16d1144c30dc3305230cbe5a8c77cc84",
+    "url": "./static/media/minimo.16d1144c.jpg"
+  }
+]);
